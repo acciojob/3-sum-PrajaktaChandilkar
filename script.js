d01630af954e5cae7d57d5faf0366adfde814ab1
PrajaktaChandilkar <95705302+PrajaktaChandilkar@users.noreply.github.com>
2022-11-18 16:44:11 +0530

    done

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-function threeSum(arr, target) {
-  //your code here
+function threeSum(arr, target){
+  closeSum = Number.MAX_VALUE;
+  let len = arr.length;
+
+  for(let i = 0; i < len; i++){
+      for(let j = i +  1; j < len; j++){
+          for(let k = j + 1; k < len; k++){
+              maxSum = arr[i] + arr[j] + arr[k];
+              if (Math.abs(maxSum - target ) < Math.abs(closeSum - target)){
+                  closeSum = maxSum;
+              }
+          }
+      }
+  }
+return closeSum;
 }
 
-module.exports = threeSum;
+//do not modify below this line
+// module.exports=threeSum;
+console.log(threeSum( [-1, 2, 1, -4], 1))
